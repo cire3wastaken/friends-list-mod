4b8dfdf47b0ee6915d0aa335ecd885f1b1bd6164
cire3 <cire3mc@gmail.com>
2024-07-08 22:36:46 -0500

    fix gradle

--- src/main/java/me/cire3/FriendsListMod.java
+++ src/main/java/me/cire3/FriendsListMod.java
@@ -1,11 +1,11 @@
-package me.cire3.friendslistmod;
+package me.cire3;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FriendsListMod implements ModInitializer {
+public class Friendslistmod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
